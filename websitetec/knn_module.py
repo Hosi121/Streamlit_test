d6632505e0b74166fee026659a56d3073a6c192a
Stern <128687243+Hosi121@users.noreply.github.com>
2023-11-08 08:36:26 +0900

    Update knn_module.py

--- websitetec/knn_module.py
+++ websitetec/knn_module.py
@@ -10,7 +10,7 @@ def init_knn(X_train, k=5):
     return knn
 
 # KNN による分類と結果の表示
-def run_knn_classification(canvas_result, knn, y_test):
+def run_knn_classification(canvas_result, knn, Y_train):
     # 描画された画像を取得
     drawn_image = canvas_result.image_data
     drawn_image_gray = drawn_image[:, :, 3]  # アルファチャンネルを使用
@@ -24,7 +24,7 @@ def run_knn_classification(canvas_result, knn, y_test):
         
         # KNN で分類
         dists, indices = knn.predict(resized_image)
-        votes = y_test[indices.flatten()]
+        votes = Y_train[indices.flatten()]
 
         # votes 配列が空でないかを確認し、空でない場合のみ処理を続行
         if len(votes) > 0:
@@ -46,10 +46,10 @@ def run_knn_classification(canvas_result, knn, y_test):
             for i, idx in enumerate(indices.flatten()):
                 with cols[i]:
                     # 画像を正しく変換
-                    pred_image = Image.fromarray(y_test[idx].reshape(28, 28), mode='L')
+                    pred_image = Image.fromarray(Y_train[idx].reshape(28, 28), mode='L')
                     pred_image = pred_image.resize((100, 100))
                     pred_image = np.array(pred_image)
-                    st.image(pred_image, clamp=True, caption=f'ラベル = {y_test[idx]}')
+                    st.image(pred_image, clamp=True, caption=f'ラベル = {Y_train[idx]}')
 
             # 正解率の表示
             st.metric(label="正解率", value=f"{accuracy:.2f}%")
