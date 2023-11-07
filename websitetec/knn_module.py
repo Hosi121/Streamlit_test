17d6786fa4d786cab1b4f896b024cf570929910e
Stern <128687243+Hosi121@users.noreply.github.com>
2023-11-08 08:07:33 +0900

    Update knn_module.py

--- websitetec/knn_module.py
+++ websitetec/knn_module.py
@@ -10,6 +10,7 @@ def init_knn(X_train, k=5):
     return knn
 
 # KNN による分類と結果の表示
+# KNN による分類と結果の表示
 def run_knn_classification(canvas_result, knn, y_test):
     # 描画された画像を取得
     drawn_image = canvas_result.image_data
@@ -21,38 +22,38 @@ def run_knn_classification(canvas_result, knn, y_test):
         drawn_image_gray = Image.fromarray(drawn_image_gray)
         resized_image = drawn_image_gray.resize((28, 28))
         resized_image = np.array(resized_image).reshape(1, -1)
+        
+        # KNN で分類
+        dists, indices = knn.predict(resized_image)
+        votes = y_test[indices.flatten()]
 
-    # KNN で分類
-    dists, indices = knn.predict(resized_image)
-    votes = y_test[indices.flatten()]
+        # votes 配列が空でないかを確認し、空でない場合のみ処理を続行
+        if len(votes) > 0:
+            prediction = np.bincount(votes).argmax()
+        
+        # 以下、予測結果と正解記録、正解率の表示など
+        else:
+            st.write("描画された内容が認識できません。もう一度描いてください。")
 
-    # votes 配列が空でないかを確認し、空でない場合のみ処理を続行
-    if len(votes) > 0:
-        prediction = np.bincount(votes).argmax()
-    
-    # 以下、予測結果と正解記録、正解率の表示など
-    else:
-        st.write("描画された内容が認識できません。もう一度描いてください。")
+            # 予測結果と正解記録の更新
+            user_prediction = st.session_state.user_prediction
+            if user_prediction == prediction:
+                st.session_state.correct += 1
+            st.session_state.attempts += 1
 
-        # 予測結果と正解記録の更新
-        user_prediction = st.session_state.user_prediction
-        if user_prediction == prediction:
-            st.session_state.correct += 1
-        st.session_state.attempts += 1
+            # 正解率の計算
+            accuracy = (st.session_state.correct / st.session_state.attempts) * 100
 
-        # 正解率の計算
-        accuracy = (st.session_state.correct / st.session_state.attempts) * 100
+            # 結果の表示
+            st.write(f"あなたの予想: {user_prediction}, KNNによる判定結果: {prediction}")
+            st.write(f"類似度上位の数字:")
+            cols = st.columns(knn.k)
+            for i, idx in enumerate(indices.flatten()):
+                with cols[i]:
+                    pred_image = Image.fromarray(y_test[idx])
+                    pred_image = pred_image.resize((100, 100))
+                    pred_image = np.array(pred_image)
+                    st.image(pred_image, clamp=True, caption=f'ラベル = {y_test[idx]}')
 
-        # 結果の表示
-        st.write(f"あなたの予想: {user_prediction}, KNNによる判定結果: {prediction}")
-        st.write(f"類似度上位の数字:")
-        cols = st.columns(knn.k)
-        for i, idx in enumerate(indices.flatten()):
-            with cols[i]:
-                pred_image = Image.fromarray(y_test[idx])
-                pred_image = pred_image.resize((100, 100))
-                pred_image = np.array(pred_image)
-                st.image(pred_image, clamp=True, caption=f'ラベル = {y_test[idx]}')
-
-        # 正解率の表示
-        st.metric(label="正解率", value=f"{accuracy:.2f}%")
+            # 正解率の表示
+            st.metric(label="正解率", value=f"{accuracy:.2f}%")
