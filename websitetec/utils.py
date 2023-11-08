e3d6d6c6c69f2c0ecd689cc58005b518495a51ae
Stern <128687243+Hosi121@users.noreply.github.com>
2023-11-08 14:02:42 +0900

    Update utils.py

--- websitetec/utils.py
+++ websitetec/utils.py
@@ -1,6 +1,7 @@
 from keras.datasets import mnist
 import os
 import streamlit as st
+from tensorflow.keras.models import load_model as keras_load_model
 
 def load_mnist_data():
     _, (X_test, y_test) = mnist.load_data()
@@ -19,3 +20,8 @@ def load_css(filename):
     
     with open(css_path, 'r', encoding='utf-8') as file:
         st.markdown(f'<style>{file.read()}</style>', unsafe_allow_html=True)
+
+def load_model(model_name):
+    project_root = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(project_root, model_name)
+    return keras_load_model(model_path)
